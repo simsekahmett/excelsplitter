e9cc0d5caeecf6568bc15e7748b7bf81fd8403cd
Ahmet Burhan Simsek <Ahmet.Simsek@gbgplc.com>
2021-08-04 13:42:14 +0300

    Add project files.

---
+++ ExcelSplitter/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace ExcelSplitter
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.label9 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(462, 6);
+			this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(104, 84);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Start Splitting";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(62, 6);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(335, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(4, 9);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Excel File:";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(401, 6);
+			this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(56, 20);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Open";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(4, 56);
+			this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(91, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Split by # of rows:";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(99, 53);
+			this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(36, 20);
+			this.textBox2.TabIndex = 5;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(401, 29);
+			this.button3.Margin = new System.Windows.Forms.Padding(2);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(56, 20);
+			this.button3.TabIndex = 9;
+			this.button3.Text = "Select";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(4, 32);
+			this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(57, 13);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "Dest Path:";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(62, 29);
+			this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(335, 20);
+			this.textBox3.TabIndex = 7;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(6, 339);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(561, 23);
+			this.progressBar1.TabIndex = 10;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(4, 365);
+			this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(131, 13);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "Current Processing File #: ";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(139, 365);
+			this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(13, 13);
+			this.label5.TabIndex = 12;
+			this.label5.Text = "0";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(459, 365);
+			this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(95, 13);
+			this.label8.TabIndex = 15;
+			this.label8.Text = "Overall Progress %";
+			// 
+			// backgroundWorker1
+			// 
+			this.backgroundWorker1.WorkerReportsProgress = true;
+			this.backgroundWorker1.WorkerSupportsCancellation = true;
+			this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+			this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+			this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(4, 79);
+			this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(28, 13);
+			this.label9.TabIndex = 16;
+			this.label9.Text = "Log:";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(7, 95);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(559, 238);
+			this.richTextBox1.TabIndex = 17;
+			this.richTextBox1.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(579, 383);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label8;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+	}
+}
+
---
+++ ExcelSplitter/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ExcelSplitter
+{
+	public partial class Form1 : Form
+	{
+		private string startPath = @"C:\";
+		private string filePath_source = @"C:\";
+		private string filePath_copiedinto = Path.GetTempPath() + @"temp.xlsx";
+		private string destinationPath = @"C:\";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			log("Splitter initialized");
+		}
+
+		// start splitting excel button
+		private void button1_Click(object sender, EventArgs e)
+		{
+			log("Splitting process starting");
+			log("Creating temp excel file to " + filePath_copiedinto);
+			var app = new Microsoft.Office.Interop.Excel.Application();
+			app.DisplayAlerts = false;
+			var wb = app.Workbooks.Add(Type.Missing);
+			wb.SaveCopyAs(filePath_copiedinto);
+			wb.Close();
+			backgroundWorker1.RunWorkerAsync();
+		}
+
+
+
+		// select excel file dialog box button
+		private void button2_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog excelFileBrowserDialog = new OpenFileDialog();
+			excelFileBrowserDialog.InitialDirectory = @"C:\";
+			excelFileBrowserDialog.Filter = "Excel files|*.xlsx";
+			excelFileBrowserDialog.ShowDialog();
+			filePath_source = excelFileBrowserDialog.FileName;
+			textBox1.Text = filePath_source;
+			log("Excel file selected");
+		}
+
+		// select destination folder dialog box button
+		private void button3_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog destinationPathBrowserDialog = new FolderBrowserDialog();
+			destinationPathBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop;
+			destinationPathBrowserDialog.ShowDialog();
+			destinationPath = destinationPathBrowserDialog.SelectedPath;
+			textBox3.Text = destinationPath;
+			log("Destination path selected");
+		}
+
+		// excel file splitting background worker
+		private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+		{
+			try
+			{
+				Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+				app.DisplayAlerts = false;
+				Microsoft.Office.Interop.Excel.Workbook book = app.Workbooks.Open(filePath_source);
+				Microsoft.Office.Interop.Excel.Worksheet sheet = (Microsoft.Office.Interop.Excel.Worksheet)book.Worksheets.get_Item(1);
+
+				int iRowCount = sheet.UsedRange.Rows.Count;
+				int maxrows = int.Parse(textBox2.Text);
+				int maxloops = iRowCount / maxrows;
+				int beginrow = 2; //skipping the header row.
+
+				Microsoft.Office.Interop.Excel.Application destxlApp;
+				Microsoft.Office.Interop.Excel.Workbook destworkBook;
+				Microsoft.Office.Interop.Excel.Worksheet destworkSheet;
+				Microsoft.Office.Interop.Excel.Range destrange;
+
+
+
+				for (int i = 1; i <= maxloops; i++)
+				{
+					log("Starting to work for file number: " + i + " and starting row # from: " + beginrow);
+					Microsoft.Office.Interop.Excel.Range startCell = sheet.Cells[beginrow, 1];
+					Microsoft.Office.Interop.Excel.Range
+						endCell = sheet.Cells[beginrow + maxrows - 1, 3]; 
+					Microsoft.Office.Interop.Excel.Range rng = sheet.Range[startCell, endCell];
+					rng = rng.EntireRow; 
+					rng.Copy(Type.Missing);
+
+					//opening of the second worksheet and pasting
+					string destPath = filePath_copiedinto;
+					destxlApp = new Microsoft.Office.Interop.Excel.Application();
+					destxlApp.DisplayAlerts = true;
+					destxlApp.Visible = true;
+					destworkBook = destxlApp.Workbooks.Open(destPath, 0, false);
+					destworkSheet = destworkBook.Worksheets.get_Item(1);
+					destrange = destworkSheet.Cells[2, 1];
+					destrange.Select();
+					Thread.Sleep(1000);
+					destworkSheet.Paste(Type.Missing, Type.Missing);
+					Thread.Sleep(1000);
+					destworkBook.SaveAs(destinationPath + "\\" + i + ".xlsx");
+					Thread.Sleep(1000);
+					beginrow = beginrow + maxrows;
+					backgroundWorker1.ReportProgress(i, maxloops);
+
+					GC.Collect();
+					GC.WaitForPendingFinalizers();
+
+					Marshal.FinalReleaseComObject(destrange);
+					Marshal.FinalReleaseComObject(destworkSheet);
+
+					destworkBook.Close(true, null, null);
+					Marshal.FinalReleaseComObject(destworkBook);
+
+					destxlApp.Quit();
+					Marshal.FinalReleaseComObject(destxlApp);
+				}
+			}
+			catch (Exception ex)
+			{
+				backgroundWorker1.CancelAsync();
+				log("Splitting process failed: " + ex.Message);
+			}
+		}
+
+		private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+		{
+			int maxLoops = (int) e.UserState;
+			progressBar1.Value = (e.ProgressPercentage / maxLoops)*100;
+			label8.Text = "Overall Progress %" + (e.ProgressPercentage / maxLoops) * 100;
+			label5.Text = e.ProgressPercentage.ToString();
+		}
+
+		private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+		{
+			log("Splitting process completed");
+		}
+
+		private void log(string log)
+		{
+			string formattedLogText = DateTime.Now.ToShortTimeString() + " ---- " + log;
+			BeginInvoke((MethodInvoker)delegate
+			{
+				richTextBox1.Text += formattedLogText + Environment.NewLine;
+			});
+		}
+	}
+}
